d022ef0582f13fcb9a35457362589f1d66d07493
joyvinab49 <zhixinwolail@163.com>
2021-02-07 14:17:15 +0800

    09

---
+++ 9 printing, printing, printing.py
@@ -0,0 +1,13 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan \nFeb \nMar \nApr \nMay \nJun \nJul \nAug" #\n是换行符
+
+print("Here are the days:", days)
+print("Here are the months:", months)
+
+#三个双引号 里面的内容可以写成多行的
+print("""
+    There's something going on here.
+    With the three double-quotes.
+    We'll be able to type as much as we like.
+    Even 4 lines if we want, or 5, or 6.
+    """)
