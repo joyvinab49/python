c364e8b6e9f000f31898214e9b41305a0f0ab9ee
joyvinab49 <zhixinwolail@163.com>
2021-02-07 10:23:53 +0800

    05

--- 5 more variables&printing.py
+++ 5 more variables&printing.py
@@ -1,17 +1,19 @@
-my_name = 'Zed A. Shaw'
-my_age = 35
-my_height = 74
-my_weight = 180
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Zed A. Shaw'
+age = 35
+height = 74
+height_cm = round(height * 2.54) #去掉小数点
+weight = 180
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
 
 #这里f和后面的不能有空格不然会报错
-print(f"Let's talk about {my_name}.")
-print(f"He's {my_height} inches tall.")
-print(f"He's {my_weight} pounds heavy.")
-print(f"He's got {my_eyes} eyes and {my_hair} hair.")
-print(f"His teeth are usually {my_teeth} depending on the coffee.")
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {height_cm} centimeters tall.")
+print(f"He's {weight} pounds heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
 
-total = my_age + my_height + my_weight
-print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
