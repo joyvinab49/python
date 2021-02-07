91ca54e8bd88606bacb074fa6b06a0e502433284
joyvinab49 <zhixinwolail@163.com>
2021-02-07 10:18:33 +0800

    05

--- 5 more variables&printing.py
+++ 5 more variables&printing.py
@@ -6,11 +6,12 @@ my_eyes = 'Blue'
 my_teeth = 'White'
 my_hair = 'Brown'
 
-print(f "Let's talk about {my_name}.")
-print(f "He's {my_height} inches tall.")
-print(f "He's {my_weight} pounds heavy.")
-print(f "He's got {my_eyes} eyes and {my_hair} hair.")
-print(f "His teeth are usually {my_teeth} depending on the coffee.")
+#这里f和后面的不能有空格不然会报错
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
 
 total = my_age + my_height + my_weight
-print(f "If I add {my_age}, {my_height}, and {my_weight} I get {toal}.")
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
