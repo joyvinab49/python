e59b3e67149cc83bb4c410d357e1049e1a39ab93
joyvinab49 <zhixinwolail@163.com>
2021-02-07 16:27:24 +0800

    12

---
+++ 12 prompting people.py
@@ -0,0 +1,9 @@
+# 和11内容一样 但是写法更方便了
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+#会先出现一个空白的input 再将下面的内容在新的一行print出来
+print("What's your name?",input())
