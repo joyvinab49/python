f034aea40f30e30444a17d4df5ab93d9b5f62079
joyvinab49 <zhixinwolail@163.com>
2021-02-07 14:17:21 +0800

    08

--- 8 printing, printing.py
+++ 8 printing, printing.py
@@ -3,7 +3,7 @@ formatter = "{} {} {} {}"
 print(formatter.format(1, 2, 3, 4))
 print(formatter.format("one", "two", "three", "four"))
 print(formatter.format(True, False, False, True))
-#下面这个输出的是16个{}
+# 下面这个输出的是16个{}
 print(formatter.format(formatter, formatter, formatter, formatter))
 print(formatter.format(
     "Hello hello,",
