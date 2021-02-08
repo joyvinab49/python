a38c9dcdc8db33a2145a7fffc36a29582035678c
joyvinab49 <zhixinwolail@163.com>
2021-02-08 16:01:39 +0800

    04

--- 04 variables&names.py
+++ 04 variables&names.py
@@ -5,7 +5,7 @@ cars_not_driven = car - drivers
 cars_driven = drivers
 
 print("There are", car, "cars available.")
-print("There are only", drivers,"drivers available.")
-print("There will be", cars_not_driven,"empty cars today.")
-print("There are", cars_driven,"driven cars today.")
-print("The total space in cars is",car*space_in_car)
+print("There are only", drivers, "drivers available.")
+print("There will be", cars_not_driven, "empty cars today.")
+print("There are", cars_driven, "driven cars today.")
+print("The total space in cars is", car * space_in_car)
