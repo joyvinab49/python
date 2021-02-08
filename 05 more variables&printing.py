2dbf47af5a42896ceacbd6cdf31ec976ef82db1a
joyvinab49 <zhixinwolail@163.com>
2021-02-08 16:03:42 +0800

    05

--- 05 more variables&printing.py
+++ 05 more variables&printing.py
@@ -1,13 +1,13 @@
 name = 'Zed A. Shaw'
 age = 35
 height = 74
-height_cm = round(height * 2.54) #去掉小数点
+height_cm = round(height * 2.54)  # 去掉小数点
 weight = 180
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
 
-#这里f和后面的不能有空格不然会报错
+# 这里f和后面的不能有空格不然会报错
 print(f"Let's talk about {name}.")
 print(f"He's {height} inches tall.")
 print(f"He's {height_cm} centimeters tall.")
