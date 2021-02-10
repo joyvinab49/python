ce065447e5e8a865d2d657077b137d45c9f018fe
joyvinab49 <zhixinwolail@163.com>
2021-02-10 14:51:35 +0800

    34

---
+++ 34 Accessing elements of lists.py
@@ -0,0 +1,18 @@
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+"""the animal at 1 is the second animal and is python"""
+print(animals[1])
+"""the third animal is at 2 and is peacock"""
+print(animals[2])
+"""the first animal is at 0 and is bear"""
+print(animals[0])
+"""the animal at 3 is the fourth animal and is kangaroo"""
+print(animals[3])
+"""the fifith animal is at 4 and is whale"""
+print(animals[4])
+"""the animal at 2 is the third animal and is peacock"""
+print(animals[2])
+"""the sixth animal is the animal at 5 and is platypus"""
+print(animals[5])
+"""the animal at 4 is the fifith animal and is whale"""
+print(animals[4])
